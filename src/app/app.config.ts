18756cb59f1fe5f2d2e9a71653f98e25d4130541
byronRC10 <byronredondo29@gmail.com>
2025-07-27 11:21:37 -0600

    commit 1 dev

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // 👈 necesario para peticiones HTTP
+import { provideHttpClient } from '@angular/common/http'; // primer commit dev 1
 
 import { routes } from './app.routes';
 
